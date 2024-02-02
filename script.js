b86677f5ea5319ce2b7119e78c9767ec4373783a
yGodaTx <156397451+yGodaTx@users.noreply.github.com>
2024-02-02 06:32:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var nome = prompt("Olá, Poderia escrever o nome da minha amada?")
+alert(nome + " é o nome com quem irei me casar um dia💞💍")
+var amor = prompt('Ja que passou da primeira etapa, poderia falar "eu te amo"')
+alert('Eu também te amo minha vida❤')
